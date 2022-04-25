761320c8cb57dc4c51e06fa33d759ecc8dfd0c9a
何顺江 <H18383056656@outlook.com>
2022-04-25 21:42:19 +0800

    第一次测试提交

---
+++ Hesj.java
@@ -0,0 +1,11 @@
+/**
+ * @author Hesj
+ * @date 2022.04.25 21:38
+ */
+public class Hesj {
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+    }
+}
